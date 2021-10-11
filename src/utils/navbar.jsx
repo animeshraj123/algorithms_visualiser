a23ffccf2d17711d3c25d6ac305fa10bdbaf2c00
Animesh Raj <65447444+animeshraj123@users.noreply.github.com>
2021-10-11 14:12:49 +0530

    Update navbar.jsx

--- src/utils/navbar.jsx
+++ src/utils/navbar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => (
   <div className="bg-danger py-2 px-2">
     <span>
       <div className="btn-sm text-decoration-none bg-danger rounded-0 text-light">
-      <a className="btn-sm text-decoration-none bg-danger rounded-0 text-light" href="https://animeshraj123.github.io/algorithms_visualiser/#/">
+      <a className="btn-sm text-decoration-none bg-danger rounded-0 text-light" href="https://github.com/animeshraj123/algorithms_visualiser">
         &nbsp; Algorithms Visualiser
       </a>
       </div>
